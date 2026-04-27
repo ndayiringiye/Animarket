ee321f3cfcc1f41807543c4cf48d547af1b9997b
David <ndayiringiyedavid120@gmail.com>
2026-04-27 12:34:13 -0700

    dev:// animal controller functionality updated

--- Backend/src/controllers/animals/animalController.js
+++ Backend/src/controllers/animals/animalController.js
@@ -1,7 +1,9 @@
 import { AnimalService } from "../../services/animals/AnimalsService.js";
 import Animal from "../../models/animals/AnimalModel.js";
+import getAllAnimals from "../../services/animals/AnimalsService.js"
+import { getAnimalById } from "../../services/animals/AnimalsService.js";
 
-const animalRegitering = async (req , res) =>{
+export const animalRegitering = async (req , res) =>{
     const animal = await Animal.AnimalService(req.body);
     try {
         return res.json({
@@ -18,4 +20,38 @@ const animalRegitering = async (req , res) =>{
     }
 }
 
-export default  animalRegitering;
+
+export const getAnimals = async (req, res )=>{
+    const animal =  await  Animal.getAllAnimals(req.body);
+    try {
+        return res.json({
+            message: "All Animal are getted successfully",
+            status: 500,
+            data: animal
+        })
+    } catch (error) {
+        return res.json({
+            message:"getting animal failed",
+            status : 404,
+            error : error.message
+        })
+    }
+}
+
+export const gettingSingleAnimal = sync (req, res) => {
+    const {id} = req.params;
+    try {
+        const animal =  await  Animal.getAnimalById(id)
+         return res.json({
+            message: "All Animal are getted successfully",
+            status: 500,
+            data: animal
+        })
+    } catch (error) {
+         return res.json({
+            message:"getting animal failed",
+            status : 404,
+            error : error.message
+        })
+    }
+}
--- Backend/src/models/Agreements/agreementModel.js
+++ Backend/src/models/Agreements/agreementModel.js
@@ -2,8 +2,16 @@ import mongoose, { Schema } from "mongoose";
 
 const agreementSchema =  mongoose,Schema({
     name:{
-        type: String
+        type: String,
+        required: true,
+        length: 30
+    },
+    user:{
+        type: String,
+        reqiured : true,
+        id: Object
     }
+    
 })
 
 export default agreementSchema;
