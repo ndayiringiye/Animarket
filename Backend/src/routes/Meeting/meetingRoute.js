4e00aaa63b49fcb9f1c05c02f61d52f90cfc82f5
David <ndayiringiyedavid120@gmail.com>
2026-05-09 01:37:50 -0700

    feat://meeting validator provide successfully

--- Backend/src/routes/Meeting/meetingRoute.js
+++ Backend/src/routes/Meeting/meetingRoute.js
@@ -12,22 +12,31 @@ import {
 } from "../../controllers/Meeting/MeetingController.js";
 
 import { authenticateUser } from "../../Middlewares/Auth/authMiddleware.js";
+import {
+    validateCreateMeeting,
+    validateMeetingId,
+    validateFeedback
+} from "../../validators/meetingValidator.js";
 
 const router = express.Router();
 
 router.use(authenticateUser);
 
-// Main Routes
-router.post("/", createMeetingController);
+
+router.post("/", validateCreateMeeting, createMeetingController);
+
 router.get("/", getUserMeetingsController);
-router.get("/:meetingId", getSingleMeetingController);
 
-router.put("/:meetingId/accept", acceptMeetingController);
-router.post("/:meetingId/join", joinMeetingController);     // Changed to POST (better practice)
-router.put("/:meetingId/start", startMeetingController);
-router.put("/:meetingId/end", endMeetingController);
-router.put("/:meetingId/cancel", cancelMeetingController);
+router.get("/:meetingId", validateMeetingId, getSingleMeetingController);
 
-router.post("/:meetingId/feedback", addFeedbackController);
+router.put("/:meetingId/accept", validateMeetingId, acceptMeetingController);
+
+router.post("/:meetingId/join", validateMeetingId, joinMeetingController);
+
+router.put("/:meetingId/start", validateMeetingId, startMeetingController);
+router.put("/:meetingId/end", validateMeetingId, endMeetingController);
+router.put("/:meetingId/cancel", validateMeetingId, cancelMeetingController);
+
+router.post("/:meetingId/feedback", validateMeetingId, validateFeedback, addFeedbackController);
 
 export default router;
